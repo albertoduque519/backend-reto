5aa42bfe0da70a27064ad1ced1889eeffdbe9171
alberto Duque <desarrollo.tinger@gmail.com>
2020-04-22 05:33:52 -0500

    replace services >

--- src/routes/statsController.js
+++ src/routes/statsController.js
@@ -3,5 +3,10 @@ const statsService = require('../services/Stats')
 exports.getDiskSpaceStatsByClient = async (req, res) => {
   //let stats = await statsService.getDiskSpaceStatsByClient(req.query.idClientes)
   let stats = [{ "tipo": "bd", "tamano": "71674688136" }, { "tipo": "logs", "tamano": "414449665" }]
+  if (req.query.idClientes.length > 1)
+    stats = [{ "tipo": "bd", "tamano": "91674688136" }, { "tipo": "logs", "tamano": "114449665" }]
+  if (req.query.idClientes.length > 2)
+    stats = [{ "tipo": "bd", "tamano": "56644688146" }, { "tipo": "logs", "tamano": "214449665" }]
+
   res.json(stats);
 };
