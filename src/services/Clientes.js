e9610bbcea348afff74a16a56680099b43221d44
alberto <duque.alberto>
2020-08-30 23:07:40 -0500

    Ajustes backend
    ajustes en el backend

--- src/services/Clientes.js
+++ src/services/Clientes.js
@@ -1,32 +1,38 @@
-const ServiceError = require('../helpers/error')
-const Clientes = require('../models/Clientes')
+const ServiceError = require("../helpers/error");
+const Clientes = require("../models/Clientes");
 
 const getClients = async function (user) {
-  let data = { attributes: ['id', 'empresa', 'capacidad_total'], raw: true }
-  if (user.empresa !== 'solati') {
+  let data = { attributes: ["id", "empresa", "capacidad_total"], raw: true };
+  if (user.empresa !== "empresa") {
     data = {
-      where: { empresa: toCamelCase(user.empresa) }, attributes: ['id', 'empresa', 'capacidad_total']
-    }
+      where: { empresa: toCamelCase(user.empresa) },
+      attributes: ["id", "empresa", "capacidad_total"],
+    };
   }
-  let result = await Clientes.findAll(data)
-    .catch(err => {
-      console.log(err)
-    })
-  return result.filter(item => item.empresa != null).map(item => {
-    if (item.empresa != null)
-      return {
-        id: item.id,
-        cliente: item.empresa,
-        capacidadTotal: item.capacidad_total
-      }
-  }) || false
-}
+  let result = await Clientes.findAll(data).catch((err) => {
+    console.log(err);
+  });
+  return (
+    result
+      .filter((item) => item.empresa != null)
+      .map((item) => {
+        if (item.empresa != null)
+          return {
+            id: item.id,
+            cliente: item.empresa,
+            capacidadTotal: item.capacidad_total,
+          };
+      }) || false
+  );
+};
 
 const toCamelCase = function (str) {
-  return str.split(' ').map(function (word, index) {
-    return word.charAt(0).toUpperCase() + word.slice(1).toLowerCase();
-  }).join('');
-}
+  return str
+    .split(" ")
+    .map(function (word, index) {
+      return word.charAt(0).toUpperCase() + word.slice(1).toLowerCase();
+    })
+    .join("");
+};
 
-
-module.exports = { getClients }
+module.exports = { getClients };
